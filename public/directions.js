ebd5406731a70a0e06885a4e0d45ea2748febeb1
David Mollert <blueskies@carolina.rr.com>
2017-08-03 18:49:41 -0400

    Add files via upload

---
+++ public/directions.js
@@ -0,0 +1,11 @@
+
+//$("#projects").on("click", function(event) {
+//	event.preventDefault();
+//	alert("This does work");
+//	$.ajax("/try", (
+//		type: "GET";
+//		dataType: "json",
+//		success: function(data) { if (callback) callback(data); },
+//		error: function() { if (callback) callback(null); },	
+//	});
+//});
